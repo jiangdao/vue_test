33de219d8154986214e4ba755c1aee90f07faab1
jiangdao <jiang_dao@126.com>
2022-06-18 10:51:12 +0800

    登录以后主页页面布局

--- src/router/index.js
+++ src/router/index.js
@@ -22,6 +22,11 @@ const routes = [
     path: '/login',
     name: 'Login',
     component: () => import( '../views/Login.vue')
+  },
+  {
+    path: '/index',
+    name: 'Index',
+    component: () => import( '../views/Index.vue')
   }
 
 ]
---
+++ src/views/Index.vue
@@ -0,0 +1,127 @@
+<template>
+  <el-container>
+    <el-aside width="200px" >
+      <el-menu
+          default-active="2"
+          class="el-menu-vertical-demo"
+          @open="handleOpen"
+          @close="handleClose"
+          background-color="#545c64"
+          text-color="#fff"
+          active-text-color="#ffd04b"
+          height="100vh">
+        <el-submenu index="1">
+          <template slot="title">
+            <i class="el-icon-location"></i>
+            <span>导航一</span>
+          </template>
+          <el-menu-item-group>
+            <template slot="title">分组一</template>
+            <el-menu-item index="1-1">选项1</el-menu-item>
+            <el-menu-item index="1-2">选项2</el-menu-item>
+          </el-menu-item-group>
+          <el-menu-item-group title="分组2">
+            <el-menu-item index="1-3">选项3</el-menu-item>
+          </el-menu-item-group>
+          <el-submenu index="1-4">
+            <template slot="title">选项4</template>
+            <el-menu-item index="1-4-1">选项1</el-menu-item>
+          </el-submenu>
+        </el-submenu>
+        <el-menu-item index="2">
+          <i class="el-icon-menu"></i>
+          <span slot="title">导航二</span>
+        </el-menu-item>
+        <el-menu-item index="3" disabled>
+          <i class="el-icon-document"></i>
+          <span slot="title">导航三</span>
+        </el-menu-item>
+        <el-menu-item index="4">
+          <i class="el-icon-setting"></i>
+          <span slot="title">导航四</span>
+        </el-menu-item>
+      </el-menu>
+    </el-aside>
+    <el-container style="height:100vh">
+      <el-header>
+        <strong>vue 后台管理系统</strong>
+        <div class="header-avatar">
+
+          <el-avatar icon="el-icon-user-solid"></el-avatar>
+
+          <el-dropdown :hide-on-click="false">
+           <span class="el-dropdown-link">
+          用户<i class="el-icon-arrow-down el-icon--right"></i>
+           </span>
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item>黄金糕</el-dropdown-item>
+              <el-dropdown-item>狮子头</el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+          <el-link href="http://www.sdpt.com.cn" target="_blank">网站</el-link>
+          <el-link href="#">视频</el-link>
+        </div>
+
+      </el-header>
+      <el-main>
+        main
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+export default {
+  methods: {
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath);
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+.el-header {
+  background-color: cadetblue;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+}
+.header-avatar{
+  float: right;
+  width:200px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+}
+.el-aside {
+  background-color: #545c64;
+  color: #333;
+  text-align: center;
+  /*height:100vh;*/
+  /*line-height: 200px;*/
+}
+
+.el-main {
+  background-color: #E9EEF3;
+  color: #333;
+  text-align: center;
+
+}
+
+body > .el-container {
+  margin-bottom: 40px;
+}
+.el-container:nth-child(5) .el-aside,
+.el-container:nth-child(6) .el-aside {
+  line-height: 260px;
+}
+
+.el-container:nth-child(7) .el-aside {
+  line-height: 320px;
+}
+</style>
